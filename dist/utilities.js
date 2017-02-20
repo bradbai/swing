741bacbeaa65e79f591a458da963a97b02f524a0
Xharmagne C <xharmagne@gmail.com>
2017-02-20 17:27:50 +1100

    add dist to git for now, while we are installing from github instead of npm

---
+++ dist/utilities.js
@@ -0,0 +1,28 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+/**
+ * Return direct children elements.
+ *
+ * @see http://stackoverflow.com/a/27102446/368691
+ * @param {HTMLElement} element
+ * @returns {Array}
+ */
+var elementChildren = function elementChildren(element) {
+  return element.childNodes.filter(function (node) {
+    return node.nodeType == 1;
+  });
+};
+
+/**
+ * @see http://stackoverflow.com/questions/4817029/whats-the-best-way-to-detect-a-touch-screen-device-using-javascript/4819886#4819886
+ * @returns {boolean}
+ */
+var isTouchDevice = function isTouchDevice() {
+  return 'ontouchstart' in window || navigator.msMaxTouchPoints;
+};
+
+exports.elementChildren = elementChildren;
+exports.isTouchDevice = isTouchDevice;
